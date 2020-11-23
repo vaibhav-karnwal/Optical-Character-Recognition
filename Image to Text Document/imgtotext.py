b2d26ebb6eb22a0566ac77335b1ed8fca9324b18
vaibhav-karnwal <53268799+vaibhav-karnwal@users.noreply.github.com>
2020-11-23 15:36:45 +0530

    Update imgtotext.py

--- Image to Text Document/imgtotext.py
+++ Image to Text Document/imgtotext.py
@@ -1,5 +1,9 @@
+import cv2
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd=r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 
-print(pytesseract.image_to_string(r'C:\Users\Er Vaibhav Karnwal\Desktop\Image to Text Document\imgtotext.png'))
+img=cv2.imread('imgtotext.png')
+img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+print(pytesseract.image_to_string(img))
